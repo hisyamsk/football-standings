3f91c3356a700725898a20d2ffcbf61476ed5843
hisyamsk <hisyamskurniawan18@gmail.com>
2021-11-09 11:50:09 +0700

    adjust table responsiveness

--- src/components/LeagueTable.js
+++ src/components/LeagueTable.js
@@ -1,11 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 
 const url = "https://api-football-standings.azharimm.site/leagues/";
+const imgAlt =
+  "https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/default-team-logo-500.png&w=100&h=100";
+
+const useWindowSize = () => {
+  const [smallSize, setSmallSize] = useState(false);
+
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      if (window.innerWidth < 768) {
+        setSmallSize(true);
+      } else {
+        setSmallSize(false);
+      }
+    };
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return smallSize;
+};
 
 const LeagueTable = ({ season, id }) => {
   const [loading, setLoading] = useState(true);
   const [standings, setStandings] = useState([]);
-  const [thead, setThead] = useState([]);
+  const smallSize = useWindowSize();
 
   const fetchData = async () => {
     try {
@@ -40,7 +60,7 @@ const LeagueTable = ({ season, id }) => {
       // })
 
       setStandings(data.standings);
-      setThead(["Rank", "Club", "MP", "W", "D", "L", "GF", "GA", "GD", "PTS"]);
+      console.log(data.standings);
     } catch (error) {
       console.log(error);
     }
@@ -48,47 +68,71 @@ const LeagueTable = ({ season, id }) => {
 
   useEffect(() => {
     fetchData();
-    console.log(season);
   }, [season]);
 
   return (
-    <table className="table">
+    <table className="table overflow-auto">
       <thead>
         <tr>
-          {thead.map((item, index) => {
-            return (
-              <th scope="col" key={index}>
-                {item}
-              </th>
-            );
-          })}
+          <th scope="col">Club</th>
+          <th scope="col">MP</th>
+          <th scope="col">W</th>
+          <th scope="col">D</th>
+          <th scope="col">L</th>
+          {!smallSize ? (
+            <>
+              <th scope="col">GF</th>
+              <th scope="col">GA</th>
+              <th scope="col">GD</th>
+            </>
+          ) : (
+            <></>
+          )}
+          <th scope="col">PTS</th>
         </tr>
       </thead>
       <tbody>
         {standings.map((club, index) => {
           return (
             <tr key={index}>
-              <th scope="row">{index + 1}</th>
-              <td>
+              <th scope="row" className="fw-normal">
+                <span
+                  className="align-middle me-1 rank"
+                  style={{
+                    minWidth: "19px",
+                    display: "inline-block",
+                  }}
+                >
+                  {index + 1}
+                </span>
                 <img
-                  src={club.team.logos[0].href}
+                  src={club.team.logos ? club.team.logos[0].href : imgAlt}
                   alt={club.team.displayName}
-                  className="me-2"
-                  style={{ height: "1.7rem" }}
+                  className="ps-2 me-2 club-img"
+                  style={{
+                    height: "1.7rem",
+                    borderLeft: club.note
+                      ? `3px solid ${club.note.color}`
+                      : "3px solid white",
+                  }}
                 />
                 <span className="align-middle">
-                  {window.innerWidth > 768
-                    ? club.team.displayName
-                    : club.team.abbreviation}
+                  {!smallSize ? club.team.displayName : club.team.abbreviation}
                 </span>
-              </td>
+              </th>
               <td>{club.stats[3].value}</td>
               <td>{club.stats[0].value}</td>
               <td>{club.stats[2].value}</td>
               <td>{club.stats[1].value}</td>
-              <td>{club.stats[4].value}</td>
-              <td>{club.stats[5].value}</td>
-              <td>{club.stats[9].value}</td>
+              {!smallSize ? (
+                <>
+                  <td>{club.stats[4].value}</td>
+                  <td>{club.stats[5].value}</td>
+                  <td>{club.stats[9].value}</td>
+                </>
+              ) : (
+                <></>
+              )}
               <td>{club.stats[6].value}</td>
             </tr>
           );
