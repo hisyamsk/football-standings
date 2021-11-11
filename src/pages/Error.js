047dbeecfc8d7d5a775b17fcd10bc2ff5e465eae
hisyamsk <hisyamskurniawan18@gmail.com>
2021-11-11 11:10:28 +0700

    updated Error Component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <h1>Error Component</h1>;
+  return (
+    <div
+      className="container my-5 shadow align-middle p-3"
+      style={{ minHeight: "500px" }}
+    >
+      <h1 className="text-center mt-5">Page not found</h1>
+      <div className="text-center">
+        <Link to="/">
+          <button className="btn btn-lg btn-secondary my-3">Home</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
